4a5e0d171e9242dd8d8c5e78abce3eace49c789f
Felix Hoffmann <fx.hoffmann93@gmail.com>
2025-07-17 10:04:36 +0200

    feat: add a config file

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// Configuration for Sheet Reminder Bot
+const spreadsheetId = "1Rmh1Jci1w6Syw31p3Wkfzuf4mPSfaebVpZvgnGZnhG4";
+const sheetName = "Sheet1";
+const dateColumn = 1;
+const actionColumn = 2;
+const startRow = 2;
